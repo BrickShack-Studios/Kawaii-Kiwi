ec1f7537266d70e58f39854541e38816d95e274f
Far117 <frank.rodriguez1337@gmail.com>
2019-03-15 04:59:14 -0400

    Rudimentary Animations
    Also added sprinting. There's a mess of model types while I work out how to best handle the Blender -> Godot workflow.

--- game/Entities/Player/Player.gd
+++ game/Entities/Player/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 export (float) var moveSpeed    : float
+export (float) var speedPercent : float
 export (float) var jumpForce    : float
 export (float) var gravity      : float
 export (float) var maxFallSpeed : float
@@ -12,7 +13,9 @@ export (float) var hLookSensitivity : float
 # Points up
 const Y_AXIS := Vector3(0, 1, 0)
 
-onready var camBase := $CameraBase as Spatial
+onready var camBase      := $CameraBase as Spatial
+onready var anim         := $AnimationPlayer/AnimationTree as AnimationTree
+onready var animPlayback := anim.get("parameters/playback") as AnimationNodeStateMachinePlayback
 
 var yVelocity  := 0.0
 var doubleJump := false
@@ -20,7 +23,8 @@ var velocity   := Vector3()
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
-	
+	anim.active = true
+	animPlayback.start("idle")
 	return
 
 func _process(delta : float) -> void:
@@ -44,7 +48,14 @@ func _physics_process(delta : float) -> void:
 	# Align movement to player rotation
 	moveVec = moveVec.rotated(Y_AXIS, rotation.y)
 	
-	moveVec *= moveSpeed
+	var runMod := 1.0
+	if (Input.is_action_pressed("run")):
+		runMod += speedPercent
+		playAnimation("run")
+	elif (velocity.length() > 0.11): playAnimation("walk")
+	elif (velocity.length() < 0.11): playAnimation("idle")
+	
+	moveVec *= moveSpeed * runMod
 	moveVec.y = yVelocity
 	
 	velocity = lerp(velocity, moveVec, drag)
@@ -52,13 +63,17 @@ func _physics_process(delta : float) -> void:
 	
 	var onFloor    : bool = is_on_floor()
 	
+	if (not onFloor): playAnimation("falling")
+	
 	yVelocity -= gravity
 	
 	if (onFloor and Input.is_action_just_pressed("jump")):
 		yVelocity  = jumpForce
+		playAnimation("jump")
 	elif (Input.is_action_just_pressed("jump") and not onFloor and not doubleJump):
 		yVelocity  = jumpForce
 		doubleJump = true
+		playAnimation("jump")
 	elif (onFloor and yVelocity <= 0):
 		yVelocity  = -0.1
 		doubleJump = false
@@ -74,3 +89,14 @@ func _input(event : InputEvent) -> void:
 		camBase.rotation_degrees.x = clamp(camBase.rotation_degrees.x, -90, 90)
 		
 		rotation_degrees.y -= event.relative.x * hLookSensitivity
+
+# Handles playing animations so you don't have to
+# worry about checking if it's already playing
+func playAnimation(a : String) -> void:
+	if (not animPlayback.is_playing()):
+		animPlayback.travel(a)
+	else:
+		if (animPlayback.get_current_node() != a):
+			animPlayback.travel(a)
+	
+	return
